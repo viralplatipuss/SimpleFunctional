2a1ba93a9f95b4a5f896e3840c946e22b4bb21d3
Dom Chapman <viralplatipuss@gmail.com>
2020-06-03 08:53:45 -0700

    Make IOToken hashable

--- Sources/SimpleFunctional/IO/IOToken.swift
+++ Sources/SimpleFunctional/IO/IOToken.swift
@@ -2,7 +2,7 @@ import Foundation
 
 /// IO Tokens are received when adding an output to an IO.
 /// They can be passed into an IO to retrieve inputs.
-public struct IOToken<T: IOType> {
+public struct IOToken<T: IOType>: Hashable {
     
     /// Unique ID backing the token.
     let id: UInt
